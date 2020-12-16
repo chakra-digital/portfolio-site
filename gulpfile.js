48235ae2e5441b06da5fda5c651451c103741731
chakradojo <info@chakradojo.com>
2020-12-16 11:10:11 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var { src, dest, watch, series } = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var copy = require('gulp-copy');
+var browsersync = require('browser-sync');
+var del = require('del');
+
+var cleanDist = function (done) {
+  del.sync('dist/');
+  return done();
+};
+
+var buildStyle = function (done) {
+  return src('src/scss/**/*.scss')
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      })
+    )
+    .pipe(prefix())
+    .pipe(dest('dist/css'));
+
+  done();
+};
+
+var watchSrc = function (done) {
+  watch('src/', series(exports.default, reloadBrowser));
+  done();
+};
+
+var copyFiles = function (done) {
+  return src(['src/**/*', '!src/scss/**']).pipe(dest('dist/'));
+};
+
+var startServer = function (done) {
+  browsersync.init({
+    server: {
+      baseDir: './dist/',
+    },
+    browser: 'google chrome',
+  });
+
+  return done();
+};
+
+var reloadBrowser = function (done) {
+  browsersync.reload();
+  done();
+};
+
+exports.default = series(cleanDist, buildStyle, copyFiles);
+
+exports.watch = series(exports.default, startServer, watchSrc);
